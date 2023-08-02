3dfd4c3bf53864ef4b381d499f01f3792a9b1f61
maram-mahmoudi <marammahmoudi@aucegypt.edu>
2023-08-02 14:00:36 +0100

    cloned turtlebot package

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/maram/ros_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/maram/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/maram/catkin_ws/devel;/opt/ros/noetic' != '' else []
+underlay_workspaces = '/home/maram/ros_ws/devel;/home/maram/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/maram/ros_ws/devel;/home/maram/catkin_ws/devel;/opt/ros/noetic' != '' else []
